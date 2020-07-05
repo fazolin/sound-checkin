4278aba8d18eabc532a1e41cd990ebae04945f94
Caio Fazolin <fazolin@gmail.com>
2020-07-05 15:55:53 -0300

    Add cors module

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+
 require('dotenv').config()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.json('You are on home')
