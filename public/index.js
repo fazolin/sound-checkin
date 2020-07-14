c54747d33e18fdcf723791bdb42dd188f7234896
Caio Fazolin <fazolin@gmail.com>
2020-07-14 00:49:03 -0300

    saving audio to db

--- public/index.js
+++ public/index.js
@@ -1,63 +1,26 @@
-// geolocation
+let mic, recorder, soundFile, soundBlob, base64String
 
-if ("geolocation" in navigator) {
-    console.log('geolocation is available')
-    navigator.geolocation.getCurrentPosition((position) => {
+let data, options
 
-        const lat = position.coords.latitude
-        const lon = position.coords.longitude
+let state = 0
 
-        document.getElementById('latitude').textContent = lat.toFixed(2)
-        document.getElementById('longitude').textContent = lon.toFixed(2)
+let reader = new FileReader();
 
-        console.log('Your latitude is: ' + lat + '°')
-        console.log('Your longitude is: ' + lon + '°')
-
-        const data = {
-            lat,
-            lon
-        }
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }
-        const recordeButton = document.getElementById('recorde-button')
-        recordeButton.onclick = () => {
-            recorde()
-        }
-
-
-        const checkInButton = document.getElementById('checkin-button')
-        checkInButton.onclick = function checkIn() {
-            fetch('https://soundcheckin.herokuapp.com' + '/post', options).then(res => {
-                console.log(data)
-                document.getElementById('done').textContent = 'DONE'
-            })
-        }
-
-    })
-} else {
-    alert("I'm sorry, but geolocation services are not supported by your browser.")
-}
-
-
-// audio record and draw
-
-let mic, recorder, soundFile
-
-
-let state = 0 // mousePress will increment from Record, to Stop, to Play
 
 function setup() {
+
+    // listen record button
+    const recordeButton = document.getElementById('record-button')
+    recordeButton.onclick = () => {
+        recorde()
+    }
+
+    // create canvas 
     if (windowWidth < 800) {
         var myCanvas = createCanvas(windowWidth / 2, windowHeight / 2)
     } else {
         var myCanvas = createCanvas(windowWidth / 3, windowHeight / 3)
     }
-
     myCanvas.parent("canvas")
 
     // create an audio in
@@ -77,8 +40,6 @@ function setup() {
 }
 
 function draw() {
-
-    // clear()
     background(255, 20)
 
     let vol = mic.getLevel()
@@ -88,37 +49,97 @@ function draw() {
     ellipse(width / 2, height / 2, vol * 20000, vol * 20000)
 }
 
-function mousePressed() {
-    if (getAudioContext().state !== 'running') {
-        getAudioContext().resume()
-    }
-}
+
 
 function recorde() {
-    console.log('recorde')
-        // use the '.enabled' boolean to make sure user enabled the mic (otherwise we'd record silence)
     if (state === 0 && mic.enabled) {
 
         recorder.record(soundFile)
-        document.getElementById('recorde-button').textContent = 'Recording now! Click to stop.'
-        document.getElementById('recorde-button').style = 'background-color: red'
+
+        document.getElementById('record-button').textContent = 'Recording now! Click to stop.'
+        document.getElementById('record-button').style = 'background-color: red'
+
+        document.getElementById('done').textContent = ''
 
         state++
+
     } else if (state === 1) {
-        recorder.stop() // stop recorder, and send the result to soundFile
 
-        // background(0, 255, 0)
-        document.getElementById('recorde-button').textContent = 'Recording stopped. Click to play '
-        document.getElementById('recorde-button').style = 'background-color: green'
+        recorder.stop()
+
+        document.getElementById('record-button').textContent = 'Recording stopped. Click to play '
+        document.getElementById('record-button').style = 'background-color: green'
 
         state++
-    } else if (state === 2) {
-        soundFile.play() // play the result!
 
-        document.getElementById('recorde-button').textContent = 'Click to new recording '
-            // saveSound(soundFile, 'mySound.wav') // save file
+    } else if (state === 2) {
+
+        soundFile.play()
+
+        document.getElementById('record-button').textContent = 'Click to new recording '
+
+        document.getElementById('coordinates').style = 'visibility: visible'
+        document.getElementById('wait').textContent = 'Acquiring coordinates...'
+
+        reader.readAsDataURL(soundFile.getBlob());
+        reader.onloadend = function() {
+            base64String = reader.result;
+            console.log(base64String);
+            postData()
+        }
+
         state = 0
     }
+}
+
+
+function postData() {
+
+    if ("geolocation" in navigator) {
+        console.log('geolocation is available')
+        navigator.geolocation.getCurrentPosition((position) => {
+
+            const lat = position.coords.latitude
+            const lon = position.coords.longitude
+
+            document.getElementById('latitude').textContent = lat.toFixed(2)
+            document.getElementById('longitude').textContent = lon.toFixed(2)
+
+            console.log('Your latitude is :' + lat + '°')
+            console.log('Your longitude is: ' + lon + '°')
+
+            document.getElementById('checkin-button').style = 'visibility: visible'
+
+            data = {
+                lat,
+                lon,
+                base64String
+            }
+            options = {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            }
+
+
+            const checkInButton = document.getElementById('checkin-button')
+            checkInButton.onclick = function checkIn() {
+                console.log('posting...')
+                document.getElementById('done').textContent = 'posting...'
+                fetch('/post', options).then(res => {
+                    console.log(data)
+
+                    document.getElementById('done').textContent = 'done'
+                })
+            }
+
+        })
+    } else {
+        alert("I'm sorry, but geolocation services are not supported by your browser.")
+    }
+
 }
 
 function windowResized() {
@@ -127,5 +148,11 @@ function windowResized() {
     } else {
         resizeCanvas(windowWidth / 3, windowHeight / 3)
     }
+}
 
+
+function mousePressed() {
+    if (getAudioContext().state !== 'running') {
+        getAudioContext().resume()
+    }
 }
